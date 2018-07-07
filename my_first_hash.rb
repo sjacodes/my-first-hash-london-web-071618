2ab0243f22981d84130e9d36bb187284ad7d9218
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 08:02:13 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,7 +11,7 @@ end
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
-  shipping_manifest
+  shipping_manifest[oil paintings]
 
 end
 
