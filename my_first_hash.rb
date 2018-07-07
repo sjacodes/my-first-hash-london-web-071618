da3a012d8f9ab16377a80e7e7cb1811c82c6cdfd
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 08:01:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,11 +9,7 @@ end
 
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
